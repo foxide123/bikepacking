d16f4a3adba3a1eceb7404199c1f06333e0f1a6f
foxide123 <kuba.kolando.02.01@gmail.com>
2023-09-28 20:01:15 +0200

    adding notification service to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bikepacking/config/routes/routes.dart';
 import 'package:bikepacking/core/dependency_injection.dart' as di;
 import 'package:bikepacking/core/dependency_injection.dart';
+import 'package:bikepacking/core/strava_local_notifications.dart';
 import 'package:bikepacking/features/strava/presentation/bloc/bloc/strava_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -11,6 +12,7 @@ import "package:timezone/data/latest.dart" as tz;
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
   tz.initializeTimeZones();
+  await NotificationService().initNotification();
   await di.init();
   runApp(
     MultiProvider(
